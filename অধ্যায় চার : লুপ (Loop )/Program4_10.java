db08ff449c4cb55692758619d07930b5cc873c1a
Anas Uddin <anasuddin44@gmail.com>
2020-12-01 23:59:19 +0600

    Add files via upload

---
+++ অধ্যায় চার : লুপ (Loop )/Program4_10.java
@@ -0,0 +1,17 @@
+package ComputerProgrammingPartOne;
+
+public class Program4_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, i;
+
+		for (i = 1; i <= 20; i++) {
+			for (n = 1; n <= 10; n++) {
+				System.out.printf("%d X %d = %d\n", i, n, i * n);
+			}
+			System.out.printf("\n");
+		}
+	}
+
+}
