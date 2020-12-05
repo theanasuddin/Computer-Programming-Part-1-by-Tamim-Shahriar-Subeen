c8b51344f31d2f1869005a4adaad54ed11486cb1
Anas Uddin <anasuddin44@gmail.com>
2020-12-05 22:08:15 +0600

    Add files via upload

---
+++ Program9_1.java
@@ -0,0 +1,12 @@
+package ComputerProgrammingPartOne;
+
+public class Program9_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char country[] = { 'B', 'a', 'n', 'g', 'l', 'a', 'd', 'e', 's', 'h', '\0' };
+		String str = new String(country);
+		System.out.printf("%s\n", str);
+	}
+
+}
