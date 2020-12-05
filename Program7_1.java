827d788524cc11e59a52975e2ad7cb383877c629
Anas Uddin <anasuddin44@gmail.com>
2020-12-05 19:00:16 +0600

    Add files via upload

---
+++ Program7_1.java
@@ -0,0 +1,15 @@
+package ComputerProgrammingPartOne;
+
+public class Program7_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a, b, c;
+		a = 2.5;
+		b = 2.5;
+		c = a + b;
+		System.out.printf("%f\n", c);
+
+	}
+
+}
