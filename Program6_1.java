98628eaffd81bc13e889f4109de48a4b39169f2f
Anas Uddin <anasuddin44@gmail.com>
2020-12-05 15:27:36 +0600

    Add files via upload

---
+++ Program6_1.java
@@ -0,0 +1,18 @@
+package ComputerProgrammingPartOne;
+
+public class Program6_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ft_marks, st_marks, final_marks;
+		double total_marks;
+		
+		ft_marks = 80;
+		st_marks = 74;
+		final_marks = 97;
+		
+		total_marks = ft_marks / 4.0 + st_marks / 4.0 + final_marks / 2.0;
+		System.out.printf("%.0f\n", total_marks);
+	}
+
+}
